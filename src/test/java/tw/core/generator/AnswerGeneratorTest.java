0579cd05e1b65cf6d72b150ca2e3828f97f79a39
YKGDWADE3 <superwadeg3@gmail.com>
2018-04-21 20:27:59 +0800

    Add AnswerGeneratorTest

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,31 @@
 package tw.core.generator;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import tw.core.exception.OutOfRangeAnswerException;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+    private AnswerGenerator mAnswerGenerator;
 
+    @Mock
+    private RandomIntGenerator mRandomIntGenerator = Mockito.mock(RandomIntGenerator.class);
+
+    @Before
+    public void setUp() {
+        mAnswerGenerator = new AnswerGenerator(mRandomIntGenerator);
+    }
+
+
+    @Test
+    public void answerGeneratorTest() throws OutOfRangeAnswerException {
+        Mockito.when(mRandomIntGenerator.generateNums(9,4)).thenReturn("1 3 5 7");
+        Assert.assertEquals("1 3 5 7", mAnswerGenerator.generate().toString());
+    }
 }
 
