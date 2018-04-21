e6c57b7b0c3083c751b5ac338fb28e53bf504452
YKGDWADE3 <superwadeg3@gmail.com>
2018-04-21 20:48:14 +0800

    add AnswerTest validate method throws exception when repeat digit

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -2,7 +2,13 @@ package tw.core;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import java.util.Arrays;
 
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
@@ -20,5 +26,16 @@ public class AnswerTest {
         Assert.assertEquals("1 4 78 34", Answer.createAnswer("1 4 78 34").toString());
         Assert.assertEquals("1 4 -1 34", Answer.createAnswer("1 4 -1 34").toString());
         Assert.assertEquals("1 & -1 34", Answer.createAnswer("1 & -1 34").toString());
+    }
+
+    @Rule
+    public ExpectedException mExpectedException =  ExpectedException.none();
+
+    @Test
+    public void validateShouldThrowsExceptionWithRepeatDigit() throws OutOfRangeAnswerException {
+        mExpectedException.expect(OutOfRangeAnswerException.class);
+        mExpectedException.expectMessage("Answer format is incorrect");
+        mAnswer.setNumList(Arrays.asList(new String[]{"1", "3", "3", "5"}));
+        mAnswer.validate();
     }
 }
