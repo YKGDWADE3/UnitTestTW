9d44c4fa9228e8b5273363add8cb65db29f97fd3
YKGDWADE3 <superwadeg3@gmail.com>
2018-04-21 20:40:01 +0800

    add AnswerTest createAnswer method

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,24 @@
 package tw.core;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
+    private Answer mAnswer;
 
+    @Before
+    public void setUp() {
+        mAnswer = new Answer();
+    }
+
+    @Test
+    public void createAnswerTest() {
+        Assert.assertEquals("1 4 78 34", Answer.createAnswer("1 4 78 34").toString());
+        Assert.assertEquals("1 4 -1 34", Answer.createAnswer("1 4 -1 34").toString());
+        Assert.assertEquals("1 & -1 34", Answer.createAnswer("1 & -1 34").toString());
+    }
 }
